93fa13114ba601abf0762aa3a44ac4b9a45b8c1f
unknown <ashsrira@ASHSRIRA-WS.cisco.com>
2015-03-23 20:06:37 -0400

    completed tasks

--- main.js
+++ main.js
@@ -4,7 +4,22 @@ var express = require('express')
 var fs      = require('fs')
 var app = express()
 // REDIS
-//var client = redis.createClient(6379, '127.0.0.1', {})
+var client = redis.createClient(6379, '127.0.0.1', {})
+
+/*client.set("key", "value");
+client.get("key", function(err,value){ console.log(value)});*/
+
+ client.on("error", function (err) {
+        console.log("Error " + err);
+    });
+
+// Set a value
+client.set("string key", "Hello1 World", redis.print);
+// Get the value back
+client.get("string key", function (err, reply) {
+console.log(reply.toString());
+});
+client.quit();
 
 ///////////// WEB ROUTES
 
@@ -47,12 +62,35 @@ app.use(function(req, res, next)
 // 	}
 // })
 
+
+
+
+
+/*app.get('/', function(req, res){
+{
+res.send('hello world');
+}
+});
+app.get('/get', function(req, res){
+{
+	client.get("theKeyToHappiness", function(err,value){
+	console.log(value);
+	res.send(value);
+});
+}
+});
+app.get('/set', function(req, res){
+	client.set("theKeyToHappiness", "Don't worry, be happy");
+	client.expire("theKeyToHappiness", 10);
+	res.send('set');
+});
+*/
+
 // HTTP SERVER
-// var server = app.listen(3000, function () {
+ var server = app.listen(3000, function () {
 
-//   var host = server.address().address
-//   var port = server.address().port
-
-//   console.log('Example app listening at http://%s:%s', host, port)
-// })
+   var host = server.address().address
+   var port = server.address().port
+   console.log('Example app listening at http://%s:%s', host, port)
+ })
 
